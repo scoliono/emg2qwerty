fdd7830fe38bc69fac570b6b0c97b9f60f971019
James Shiffer <jamesnshiffer@gmail.com>
2025-03-07 17:57:52 -0800

    Added TDSLSTMEncoder class (LSTM) to modules.py

--- emg2qwerty/modules.py
+++ emg2qwerty/modules.py
@@ -9,6 +9,8 @@ from collections.abc import Sequence
 import torch
 from torch import nn
 
+import math
+
 
 class SpectrogramNorm(nn.Module):
     """A `torch.nn.Module` that applies 2D batch normalization over spectrogram
@@ -238,7 +240,31 @@ class TDSFullyConnectedBlock(nn.Module):
         x = self.fc_block(x)
         x = x + inputs
         return self.layer_norm(x)  # TNC
+    
+#module below added by sara
+class TDSLSTMEncoder(nn.Module):
+    def __init__(self, num_features: int, lstm_hidden_size: int = 128, num_lstm_layers: int = 4,) -> None:
+        super().__init__()
+        
+        #use pytorch LSTM function
+        self.lstm_layers = nn.LSTM(
+                input_size = num_features, 
+                hidden_size = lstm_hidden_size, 
+                num_layers = num_lstm_layers, 
+                batch_first = False, 
+                bidirectional = True
+            )
+        #fc block
+        self.fc_block = TDSFullyConnectedBlock(lstm_hidden_size * 2)
+        self.out_layer = nn.Linear(lstm_hidden_size * 2, num_features)
 
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        x, _ = self.lstm_layers(inputs)
+        x = self.fc_block(x)
+        x = self.out_layer(x)
+        print("shape after TDSLSTMEncoder: ", x.shape)
+        return x
+        
 
 class TDSConvEncoder(nn.Module):
     """A time depth-separable convolutional encoder composing a sequence
@@ -278,3 +304,114 @@ class TDSConvEncoder(nn.Module):
 
     def forward(self, inputs: torch.Tensor) -> torch.Tensor:
         return self.tds_conv_blocks(inputs)  # (T, N, num_features)
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, dim_model, dropout_p, max_len):
+        super().__init__()
+        # Modified version from: https://pytorch.org/tutorials/beginner/transformer_tutorial.html
+        # max_len determines how far the position can have an effect on a token (window)
+        
+        # Info
+        self.dropout = nn.Dropout(dropout_p)
+        
+        # Encoding - From formula
+        pos_encoding = torch.zeros(max_len, dim_model)
+        positions_list = torch.arange(0, max_len, dtype=torch.float).view(-1, 1) # 0, 1, 2, 3, 4, 5
+        division_term = torch.exp(torch.arange(0, dim_model, 2).float() * (-math.log(10000.0)) / dim_model) # 1000^(2i/dim_model)
+        
+        # PE(pos, 2i) = sin(pos/1000^(2i/dim_model))
+        pos_encoding[:, 0::2] = torch.sin(positions_list * division_term)
+        
+        # PE(pos, 2i + 1) = cos(pos/1000^(2i/dim_model))
+        pos_encoding[:, 1::2] = torch.cos(positions_list * division_term)
+        
+        # Saving buffer (same as parameter without gradients needed)
+        pos_encoding = pos_encoding.unsqueeze(0).transpose(0, 1)
+        self.register_buffer("pos_encoding",pos_encoding)
+        
+    def forward(self, token_embedding: torch.tensor) -> torch.tensor:
+        # Residual connection + pos encoding
+        return self.dropout(token_embedding + self.pos_encoding[:token_embedding.size(0), :])
+
+class Transformer(nn.Module):
+    def __init__(self, d_model, dropout, num_tokens, **kwargs):
+        super().__init__()
+        self.dim_model = d_model
+        self.positional_encoder = PositionalEncoding(
+            dim_model=d_model, dropout_p=dropout, max_len=5000
+        )
+        self.embedding = nn.Embedding(num_tokens, d_model)
+        self.transformer = nn.Transformer(**kwargs)
+        self.out = nn.Linear(d_model, num_tokens)
+
+    def get_tgt_mask(self, size) -> torch.tensor:
+        # Generates a squeare matrix where the each row allows one word more to be seen
+        mask = torch.tril(torch.ones(size, size) == 1) # Lower triangular matrix
+        mask = mask.float()
+        mask = mask.masked_fill(mask == 0, float('-inf')) # Convert zeros to -inf
+        mask = mask.masked_fill(mask == 1, float(0.0)) # Convert ones to 0
+        
+        # EX for size=5:
+        # [[0., -inf, -inf, -inf, -inf],
+        #  [0.,   0., -inf, -inf, -inf],
+        #  [0.,   0.,   0., -inf, -inf],
+        #  [0.,   0.,   0.,   0., -inf],
+        #  [0.,   0.,   0.,   0.,   0.]]
+        
+        return mask
+    
+    def create_pad_mask(self, matrix: torch.tensor, pad_token: int) -> torch.tensor:
+        # If matrix = [1,2,3,0,0,0] where pad_token=0, the result mask is
+        # [False, False, False, True, True, True]
+        return (matrix == pad_token)
+
+    def forward(self, src, tgt):
+        # Src size must be (batch_size, src sequence length)
+        # Tgt size must be (batch_size, tgt sequence length)
+
+        # Embedding + positional encoding - Out size = (batch_size, sequence length, dim_model)
+        src = self.embedding(src) * math.sqrt(self.dim_model)
+        tgt = self.embedding(tgt) * math.sqrt(self.dim_model)
+        src = self.positional_encoder(src)
+        tgt = self.positional_encoder(tgt)
+
+        # we permute to obtain size (sequence length, batch_size, dim_model),
+        src = src.permute(1, 0, 2)
+        tgt = tgt.permute(1, 0, 2)
+
+        # Transformer blocks - Out size = (sequence length, batch_size, num_tokens)
+        transformer_out = self.transformer(src, tgt)
+        out = self.out(transformer_out)
+
+        return out
+    
+    def predict(self, model, input_sequence, max_length=15, SOS_token=2, EOS_token=3):
+        """
+        Method from "A detailed guide to Pytorch's nn.Transformer() module.", by
+        Daniel Melchor: https://medium.com/@danielmelchor/a-detailed-guide-to-pytorchs-nn-transformer-module-c80afbc9ffb1
+        """
+        model.eval()
+        
+        SOS_token = np.array([2])
+        EOS_token = np.array([3])
+        y_input = torch.Tensor([[SOS_token]], dtype=torch.long, device=device)
+
+        num_tokens = len(input_sequence[0])
+
+        for _ in range(max_length):
+            # Get source mask
+            tgt_mask = model.get_tgt_mask(y_input.size(1)).to(device)
+            
+            pred = model(input_sequence, y_input, tgt_mask)
+            
+            next_item = pred.topk(1)[1].view(-1)[-1].item() # num with highest probability
+            next_item = torch.Tensor([[next_item]], device=device)
+
+            # Concatenate previous input with predicted best word
+            y_input = torch.cat((y_input, next_item), dim=1)
+
+            # Stop if model predicts end of sentence
+            if next_item.view(-1).item() == EOS_token:
+                break
+
+        return y_input.view(-1).tolist()
